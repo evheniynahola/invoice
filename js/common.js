1264328f3b972f908c16d23807d595564d9ac804
Evheniy Nahola <evgeniy.nagola@gmail.com>
2023-01-11 09:09:44 +0200

    update 1.2 - 10.01.2023

--- js/common.js
+++ js/common.js
@@ -1,5 +1,30 @@
-$(function() {
+document.addEventListener('DOMContentLoaded', function(){
 
-	// Custom JS
+	const choices = document.querySelectorAll('.documentData_select');
 
-});
+	choices.forEach(el => {
+		const choice = new Choices(el, {
+			itemSelectText: 'לחץ כדי לבחור',
+			noResultsText: 'לא נמצאו תוצאות',
+			searchEnabled: false
+		})
+	})
+
+	$('.documentAdd_button_getMenu').click(function(){
+		$('.documentAdd_list').slideToggle()
+	})
+
+	$('.dropDownWrapper').click(function(){
+		$(this).toggleClass('active')
+		$(this).find('.dropDownWrapper_list').slideToggle()
+	})
+
+	$('.documentAdd_button_main').click(function(){
+		$('.modal').fadeIn().css('display', 'flex')
+	})
+
+	$('.modal_close').click(function(){
+		$('.modal').fadeOut()
+	})
+	
+})
